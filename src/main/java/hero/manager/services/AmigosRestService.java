fd955342ce11bce183429510edddbbceb9368e2a
Rodrigo Grisolía <grisoliarodrigo@gmail.com>
2019-04-04 17:56:05 -0300

    Fix public class

--- src/main/java/hero/manager/services/AmigosRestService.java
+++ src/main/java/hero/manager/services/AmigosRestService.java
@@ -2,7 +2,7 @@ package hero.manager.services;
 
 import com.google.gson.JsonArray;
 
-class AmigosRestService extends RestService {
+public class AmigosRestService extends RestService {
 
 	public JsonArray getAmistades() {
 		return get("https://hero-manager-api.herokuapp.com/amistades");
